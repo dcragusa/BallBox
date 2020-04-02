013845b4d1fe179954f8f4bb7cf6f57bfbe6cf4b
dcragusa <dcragusa@yahoo.com>
2020-04-02 05:07:42 -0700

    Argparsify Clearer readme Add speed factor Add further examples

--- README.md
+++ README.md
@@ -1,16 +1,42 @@
 # BallBox
 
-A simulation of balls in a box, with inelastic collisions.
+<p align="center">
+  <img width="400" height="400" src="https://github.com/dcragusa/BallBox/blob/master/balls.gif)">
+</p>
 
-![ball simulation](balls.gif)
+A simulation of balls in a box, with inelastic collisions. Now argparse-ified!
 
-The default behaviour is to open a 1000x1000px animation with 60 randomly sized and coloured balls.
+The default behaviour is to open a 1000x1000px live animation with 60 balls. 
 
-The parameter to `Box()` controls the scale factor. If you want a smaller image, and you probably do if you want to 
-output gifs, set this to some value. For example, setting 5 will produce a 200x200px animation.
+    usage: ballbox.py [-h] [-s SIZE SIZE] [-n NUM] [--random_sizes] [--random_colours] [--speed SPEED] [--fps FPS] [-f {gif,mp4}] [-d DURATION] [-o OUTPUT]
+    
+    optional arguments:
+      -h, --help            show this help message and exit
+      -s SIZE SIZE, --size SIZE SIZE
+                            The size of the box in pixels. Default: 1000x1000 px.
+      -n NUM, --num NUM     The number of balls to simulate. Default: 60.
+      --random_sizes        If present, balls will have random sizes.
+      --random_colours, --random_colors
+                            If present, balls will have random colours.
+      --speed SPEED         The factor affecting the speed of balls in the simulation. Default: 1.
+      --fps FPS             The frames per second of the animation. The maximum is 1000fps. Default: 60 fps.
+      -f {gif,mp4}, --file_type {gif,mp4}
+                            If present, the file type of the output animation. Default: a live window animation. ImageMagick must be installed for gif output. FFmpeg must be installed for video output.
+      -d DURATION, --duration DURATION
+                            The duration in seconds of the output animation. No effect on a live window. Default: 5 sec.
+      -o OUTPUT, --output OUTPUT
+                            The filename of the output animation. No effect on a live window. Default: balls.
 
-The parameters to `add_random_balls` are the number of balls, whether the balls should be random sizes, and whether 
-they should be random colours (the default is blue).
+More examples can be found below.
 
-The parameter to `start_animation` is the gif duration in seconds. A duration of `None` produces an indefinite
-matplotlib window animation.
+    python ballbox.py -s 400 400 -n 20 --fps 30 -f gif -d 10 -o balls_simple
+    
+<p align="center">
+  <img width="400" height="400" src="https://github.com/dcragusa/BallBox/blob/master/balls_simple.gif)">
+</p>
+
+    python ballbox.py -s 400 400 -n 40 --random_sizes --random_colours --speed 1.5 -f gif -d 10 -o balls_complex
+
+<p align="center">
+  <img width="400" height="400" src="https://github.com/dcragusa/BallBox/blob/master/balls_complex.gif)">
+</p>
--- ballbox.py
+++ ballbox.py
@@ -1,10 +1,12 @@
 import math
+import argparse
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from random import random, uniform, choice
 
 mpl.use('TkAgg')
+BASE_SPEED = 10
 
 
 class Vector2D:
@@ -90,16 +92,15 @@ class Ball:
                 other.v = v2_normal_vector_after + v2_tangent_vector_after
                 self.in_collision.append(other)
                 other.in_collision.append(self)
-        else:
-            if other in self.in_collision:
-                self.in_collision.remove(other)
-                other.in_collision.remove(self)
+        elif other in self.in_collision:
+            self.in_collision.remove(other)
+            other.in_collision.remove(self)
 
 
 class Box:
-    def __init__(self, factor=1):
-        self.factor = factor
-        self.size_x, self.size_y = 1000 / factor, 1000 / factor
+    def __init__(self, x, y):
+        self.size_factor = min(x, y) / 1000
+        self.size_x, self.size_y = x, y
         self.balls = []
         self.fig, self.ax = plt.subplots()
         dpi = 100
@@ -108,7 +109,7 @@ class Box:
         self.ax.set_xlim(0, self.size_x), self.ax.set_xticks([])
         self.ax.set_ylim(0, self.size_y), self.ax.set_yticks([])
         self.artist_to_ball = {}
-        self.fig.tight_layout()
+        plt.subplots_adjust(left=0, bottom=0, right=1, top=1, wspace=0, hspace=0)
 
     def add_ball(self, x, y, r, vx, vy):
         self.balls.append(ball := Ball(x, y, r, vx, vy))
@@ -118,21 +119,21 @@ class Box:
         for ball in ball_list:
             self.add_ball(*ball)
 
-    def add_random_balls(self, num, random_sizes=True, random_colours=True):
-        j = 0
-        for i in range(num):
+    def add_random_balls(self, num, random_sizes=True, random_colours=True, speed_factor=1.0):
+        i, j = 0, 0
+        while i < num:
             if j > num * 5:
                 break
             size = choice([25, 30, 35]) if random_sizes else 20
-            size = size / self.factor
+            size = size * self.size_factor
             color = [random() for _ in range(3)] if random_colours else 'b'
+            speed = BASE_SPEED * speed_factor * self.size_factor
             ball = Ball(
                 x := uniform(size, self.size_x-size), y := uniform(size, self.size_y-size),
-                size, uniform(-7, 7) / self.factor, uniform(-7, 7) / self.factor
+                size, uniform(-speed, speed), uniform(-speed, speed)
             )
-            if any([ball.is_colliding(existing_ball) for existing_ball in self.balls]):
-                i -= 1
-            else:
+            if not any([ball.is_colliding(existing_ball) for existing_ball in self.balls]):
+                i += 1
                 self.balls.append(ball)
                 self.artist_to_ball[self.ax.add_artist(plt.Circle((x, y), size, color=color))] = ball
             j += 1
@@ -147,33 +148,78 @@ class Box:
     def update_fig(self, _, num_steps):
         for _ in range(num_steps):
             self.time_step(num_steps)
-        circles = []
         for artist in self.ax.get_children():
             ball = self.artist_to_ball.get(artist, None)
             if ball:
                 artist.set_center((ball.x, ball.y))
-                circles.append(artist)
-        return circles
 
-    def start_animation(self, gif_duration=None):
-        if gif_duration:
-            num_steps = 5
-            frames = 60 * gif_duration
-        else:
-            num_steps = 5
-            frames = None
+    def start_animation(self, file_type, fps, duration, filename):
+
+        frames = fps * duration if file_type else None
+        interval = 1000 / fps
+        if file_type:
+            # annoyingly required as file animations don't respect the interval set in FuncAnimation, so the balls
+            # bounce around at sanic speed at high fps.
+            factor = 60 / fps
+            for ball in self.balls:
+                ball.v *= factor
+
         anim = animation.FuncAnimation(
-            self.fig, self.update_fig, frames=frames, interval=5, fargs=(num_steps,), blit=True
+            self.fig, self.update_fig, frames=frames, interval=interval, fargs=(5,)
         )
-        if gif_duration:
+        if file_type == 'gif':
             anim.save(
-                'balls.gif', animation.ImageMagickWriter(fps=60, extra_args=['-layers', 'Optimize']),
+                f'{filename}.gif', animation.ImageMagickWriter(fps=fps, extra_args=['-layers', 'Optimize'])
             )
+        elif file_type == 'mp4':
+            anim.save(f'{filename}.mp4', animation.FFMpegWriter(fps=fps))
         else:
             plt.show()
 
 
 if __name__ == '__main__':
-    box = Box(3)
-    box.add_random_balls(60, random_sizes=True, random_colours=True)
-    box.start_animation(gif_duration=10)
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-s', '--size', nargs=2, default=[1000, 1000], type=int,
+        help='The size of the box in pixels. Default: 1000x1000 px.'
+    )
+    parser.add_argument(
+        '-n', '--num', default=60, type=int,
+        help='The number of balls to simulate. Default: 60.'
+    )
+    parser.add_argument(
+        '--random_sizes', action='store_true',
+        help='If present, balls will have random sizes.'
+    )
+    parser.add_argument(
+        '--random_colours', '--random_colors', action='store_true', dest='random_colours',
+        help='If present, balls will have random colours.'
+    )
+    parser.add_argument(
+        '--speed', default=1.0, type=float,
+        help='The factor affecting the speed of balls in the simulation. Default: 1.'
+    )
+    parser.add_argument(
+        '--fps', default=60, type=int,
+        help='The frames per second of the animation. The maximum is 1000fps. Default: 60 fps.'
+    )
+    parser.add_argument(
+        '-f', '--file_type', choices=['gif', 'mp4'],
+        help='If present, the file type of the output animation. Default: a live window animation. '
+             'ImageMagick must be installed for gif output. FFmpeg must be installed for video output.'
+    )
+    parser.add_argument(
+        '-d', '--duration', default=5, type=int,
+        help='The duration in seconds of the output animation. No effect on a live window. Default: 5 sec.'
+    )
+    parser.add_argument(
+        '-o', '--output', default='balls',
+        help='The filename of the output animation. No effect on a live window. Default: balls.'
+    )
+    args = parser.parse_args()
+
+    box = Box(args.size[0], args.size[1])
+    box.add_random_balls(
+        args.num, random_sizes=args.random_sizes, random_colours=args.random_colours, speed_factor=args.speed
+    )
+    box.start_animation(file_type=args.file_type, fps=args.fps, duration=args.duration, filename=args.output)
